c3e55496a66b91a22366160b68a82e2fa6d77df8
NicolasGHS <nicoghyselincks@outlook.be>
2024-10-17 22:38:04 +0200

    sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import { useState } from "react";
-import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
-
+import Sidebar from "./components/Sidebar";
 
 
 function App() {
-  const [open, setOpen] = useState(true);
- 
   
   // const [greetMsg, setGreetMsg] = useState("");
   // const [name, setName] = useState("");
@@ -18,9 +14,7 @@ function App() {
 
   return (
     <div className="bg-white">
-      <div className={`${open ? "w-72" : "w-20"} bg-black h-screen p-5 pt-8 relative duration-300`}>
-        <p className="text-red-50" onClick={() => setOpen(!open)}>Close</p>
-      </div>
+      <Sidebar />
       <p className="">Test</p> 
     </div>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 
 
 function Sidebar() {
-  return (
-    <div>Sidebar</div>
-  )
+    const [open, setOpen] = useState(true);
+
+
+    return (
+        <div className={`${open ? "w-72" : "w-20"} bg-black h-screen p-5 pt-8 relative duration-300`}>
+            <p className="text-red-50" onClick={() => setOpen(!open)}>Close</p>
+        </div>
+    )
 }
 
-export default Sidebar
+export default Sidebar;
